59c5844b6ee219be2aeb6ec7b302b16994111302
rafabruno <brunor_morais@hotmail.com>
2020-11-25 11:49:19 -0300

    adjusts SEO components

--- src/components/SEO/index.tsx
+++ src/components/SEO/index.tsx
@@ -25,8 +25,15 @@ function SEO({
 
       {description && <meta name="description" content={description} />}
       {image && <meta name="image" content={pageImage} />}
-
       {!shouldIndexPage && <meta name="robots" content="noindex,nofollow" />}
+
+      <meta httpEquiv="x-ua-compatible" content="IE=edge,chrome=1" />
+      <meta name="MobileOptimized" content="320" />
+      <meta name="HandheldFriendly" content="True" />
+      <meta name="theme-color" content="#0A0A0A" />
+      <meta name="msapplication-TileColor" content="#0A0A0A" />
+      <meta name="referrer" content="no-referrer-when-downgrade" />
+      <meta name="google" content="notranslate" />
     </Head>
   )
 }
